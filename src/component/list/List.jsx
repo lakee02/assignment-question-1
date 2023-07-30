80a08409edff5724799bfe78bd2b58d68d162101
lakee mangal <lakee.12002794@lpu.in>
2023-07-30 11:52:04 +0530

    commit the changes

---
+++ src/component/list/List.jsx
@@ -0,0 +1,36 @@
+import ListRow from "./ListRow";
+import ListRowCell from "./ListRowCell";
+
+import ListHeader from "./ListHeader";
+import ListHeaderCell from "./ListHeaderCell";
+
+import styles from "./List.module.css";
+
+const List = ({ rows ,currency}) => {
+  return (
+    <table className={styles.container}>
+      <thead>
+        <ListHeader>
+          <ListHeaderCell>Order ID</ListHeaderCell>
+          <ListHeaderCell>Buy/Sell</ListHeaderCell>
+          <ListHeaderCell>Country</ListHeaderCell>
+          <ListHeaderCell>Order Submitted</ListHeaderCell>
+          <ListHeaderCell>Order Volume / {currency}</ListHeaderCell>
+        </ListHeader>
+      </thead>
+      <tbody>
+        {rows.map((row) => (
+          <ListRow>
+            <ListRowCell>{row["&id"]}</ListRowCell>
+            <ListRowCell>{row.executionDetails.buySellIndicator}</ListRowCell>
+            <ListRowCell>{row.executionDetails.orderStatus}</ListRowCell>
+            <ListRowCell>{row.timestamps.orderSubmitted}</ListRowCell>
+            <ListRowCell>{row.bestExecutionData.orderVolume[currency]}</ListRowCell>
+          </ListRow>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default List;
